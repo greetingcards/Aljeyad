a541017bc6bcd77cbd4052ce6565d3dcd6d6ebb6
greetingcards <162377092+greetingcards@users.noreply.github.com>
2024-03-08 17:46:42 +0300

    Update newindex.js

--- js/newindex.js
+++ js/newindex.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext('2d');
 var img = new Image();
 img.crossOrigin="anonymous";
 
-window.addEventListener('load', DrawPlaceholder(img,ctx,"#000",'111.jpg',205,355));
+window.addEventListener('load', DrawPlaceholder(img,ctx,"#000",'111.jpg',205,345));
 
 
 
